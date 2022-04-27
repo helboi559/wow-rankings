1796d13d1266d36d60d33045ec78274c242b0d9e
Juan Ramirez <rami8224.2007@gmail.com>
2022-04-27 17:09:08 -0400

    added 3 endpoints for one event listener

--- main.js
+++ main.js
@@ -1,34 +1,50 @@
-//select html elements
+//FORM SUBMIT VARIABLE **********
 let submitChar = document.querySelector('#submitCharSearch input')
-let regionSelect = document.querySelector("#regionSelect")
-let serverSelect = document.querySelector('#serverSelect')
-let charName = document.querySelector('#charName input')
-let specificCharInfo = document.querySelector("#specificInfo input")
+// console.log(submitChar)
 
-// console.log(charCardInfo)
-let charImage = document.querySelector("img")
-// console.log(regionSelect)
-// let selectedRegion = document.querySelector("#selectedRegion")
 //add listener to take all info(region,name,realm,specifics)
 submitChar.addEventListener('click',(event) => {
     event.preventDefault();
+    //SELECT HTML ELEMENTS ************
+    //OPTIONS IN CHAR SEARCH ELEMENTS **
+    let regionSelect = document.querySelector("#regionSelect")
+    let serverSelect = document.querySelector('#serverSelect')
+    let charName = document.querySelector('#charName input')
+    // let specificCharInfo = document.querySelector("#specificInfo")
+    
+    //HIDDEN CARD TEMPLATE ELEMENTS TO DISPLAY IN CHAR SEARCH ELEMENTS
+    let charImage = document.querySelector("img")
     let charCardInfo = document.querySelector(".card")
     let cardTitle = document.querySelector(".card h5")
     let cardInfo = document.querySelector(".card p")
-    let cardSeasonScore = document.querySelector(".card li")
+    let cardSeasonScore = document.querySelector("#dungeonScore")
+    let cardRaidPro = document.querySelector("#raidProgression")
+    let cardGear = document.querySelector("#gear")
     // console.log(regionSelect.value)
+    
+    //GET DATA FROM API FROM 3 ENDPOINTS
     async function displayChar(){
-        let response = await fetch(`https://raider.io/api/v1/characters/profile?region=us&realm=firetree&name=fourscoops&fields=mythic_plus_scores_by_season%3Acurrent`)
+        let responseDungeon = await fetch(`https://raider.io/api/v1/characters/profile?region=${regionSelect.value}&realm=${serverSelect.value}&name=${charName.value}&fields=mythic_plus_scores_by_season%3Acurrent`)
         // https://raider.io/api/v1/characters/profile?region=${regionSelect.value}&realm=${serverSelect.value}&name=${charName.value}&fields=${specificCharInfo.value}
-        let data = await response.json();
-        console.log(typeof data.thumbnail_url)
+        let dataDungeon = await responseDungeon.json();
+
+        let responseRaid = await fetch(`https://raider.io/api/v1/characters/profile?region=${regionSelect.value}&realm=${serverSelect.value}&name=${charName.value}&fields=raid_progression`)
+        //
+        let dataRaid = await responseRaid.json()
+
+        let responseGear = await fetch(`https://raider.io/api/v1/characters/profile?region=${regionSelect.value}&realm=${serverSelect.value}&name=${charName.value}&fields=gear`)
+        //
+        let dataGear = await responseGear.json()
+        console.log(dataGear)
+
         //show info and display character info in a card(avatar,name,character description, current season score)
         charCardInfo.style.display = "block"
-        charImage.src = data.thumbnail_url
-        cardTitle.textContent = data.name
-        cardInfo.textContent = `Level 60 ${data.faction} ${data.race} ${data.active_spec_name} ${data.class}`
-        cardSeasonScore.textContent = `Season 3 score : ${data.mythic_plus_scores_by_season[0].scores.all}`
-
+        charImage.src = dataDungeon.thumbnail_url
+        cardTitle.textContent = dataDungeon.name
+        cardInfo.textContent = `Level 60 ${dataDungeon.faction} ${dataDungeon.race} ${dataDungeon.active_spec_name} ${dataDungeon.class}`
+        cardSeasonScore.textContent = `Season 3 score : ${dataDungeon.mythic_plus_scores_by_season[0].scores.all}`
+        cardRaidPro.textContent = `Current Progression: ${dataRaid.raid_progression['sepulcher-of-the-first-ones'].summary} `
+        cardGear.textContent = `Gear Score: ${dataGear.gear.item_level_equipped}`
     }
     displayChar();
 })
@@ -40,12 +56,12 @@ async function displayRio(){
     let rioResponse = await fetch('https://raider.io/api/v1/mythic-plus/runs?season=season-sl-3&region=us&dungeon=all&affixes=all&page=0')
     let rioListData = await rioResponse.json();
     //return top 20 runs for the season and score
-    console.log(rioListData)
+    // console.log(rioListData)
     
     let top20 = rioListData.rankings
     // console.log(typeof top20)
     for(let i = 0 ; i < top20.length; i++) {
-        console.log(top20[i])
+        // console.log(top20[i])
         let teamRun = top20[i]
         //convert milliseconds to minutes/seconds
         let convertTime = teamRun.run.clear_time_ms
