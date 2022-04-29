d8bd71ee8a8d32ce590cfd2321106115b3cbcce6
Juan Ramirez <rami8224.2007@gmail.com>
2022-04-29 12:29:28 -0400

    added links to leaderboard

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-//ADD Banner, fine details(servername in card etc)
-//add listener to take all info(region,name,realm,specifics) - FEATURE 1
-//form submit variable **********
+//ADD Banner, fine details(servername in card etc, add navbar, fix team roster, add tooltip?1)
+
+//create form and take user input (region,name,realm(server name)) - FEATURE 1 *******
 const characterSearch = ()=> {
+//form submit variable 
 let submitChar = document.querySelector('#submitCharSearch input')
 // console.log(submitChar)
 submitChar.addEventListener('click',(event) => {
     event.preventDefault();
-    //SELECT HTML ELEMENTS ************
-    //OPTIONS IN CHAR SEARCH ELEMENTS **
+    //SELECT HTML ELEMENTS
+    //OPTIONS IN CHAR SEARCH ELEMENTS **** FEATURE 1A
     let regionSelect = document.querySelector("#regionSelect")
     let serverSelect = document.querySelector('#serverSelect')
     let charName = document.querySelector('#charName input')
     // let specificCharInfo = document.querySelector("#specificInfo")
     
-    //HIDDEN CARD TEMPLATE ELEMENTS TO DISPLAY IN CHAR SEARCH ELEMENTS
+    //HIDDEN CARD TEMPLATE ELEMENTS TO DISPLAY IN CHAR SEARCH ELEMENTS*****  FEATURE 1A
     let charImage = document.querySelector("img")
     let charCardInfo = document.querySelector(".card")
     let cardTitle = document.querySelector(".card h5")
     let cardInfo = document.querySelector(".card p")
     let cardSeasonScore = document.querySelector("#dungeonScore")
+    
+    // HIDDEN CARD TEMPLATE ELEMENTS TO DISPLAY IN CHAR SEARCH ELEMENTS- DIFFERENT ENDPOINTS***** FEATURE 1B
     let cardRaidPro = document.querySelector("#raidProgression")
     let cardGear = document.querySelector("#gear")
     let cardIndividual = document.querySelector("#individualRuns")
@@ -38,13 +41,13 @@ submitChar.addEventListener('click',(event) => {
         
         let individualRunBest = await fetch(`https://raider.io/api/v1/characters/profile?region=${regionSelect.value}&realm=${serverSelect.value}&name=${charName.value}&fields=mythic_plus_best_runs%3Aall`)
         let dataIndividualRun = await individualRunBest.json();
-        console.log(dataIndividualRun)
+        // console.log(dataIndividualRun)
 
         //show info and display character info in a card(avatar,name,character description, current season score)
         charCardInfo.style.display = "block"
         charImage.src = dataDungeon.thumbnail_url
         cardTitle.textContent = dataDungeon.name
-        cardInfo.textContent = `Level 60 ${dataDungeon.faction} ${dataDungeon.race} ${dataDungeon.active_spec_name} ${dataDungeon.class}`
+        cardInfo.textContent = `Level 60 ${dataDungeon.faction} ${dataDungeon.race} ${dataDungeon.active_spec_name} ${dataDungeon.class}, <${dataDungeon.region}> ${dataDungeon.realm}`
         cardSeasonScore.textContent = `Season 3 score : ${dataDungeon.mythic_plus_scores_by_season[0].scores.all}`
         cardRaidPro.textContent = `Current Progression: ${dataRaid.raid_progression['sepulcher-of-the-first-ones'].summary} `
         cardGear.textContent = `Gear Score: ${dataGear.gear.item_level_equipped}`
@@ -111,18 +114,18 @@ async function displayRio(){
         }
         //create a new element and add each run details to each run
         let runListItem = document.createElement('div');
-        runListItem.className = 'row'
+        runListItem.className = 'row align-items-start'
         runListItem.innerHTML = `
         <div class="col">${teamRun.rank}</div>
         <div class="col">${teamRun.run.dungeon.short_name}</div>
         <div class="col">+${teamRun.run.mythic_level}</div>
         <div class="col">${milliToMin(convertTime)}</div>
         <div class="col">${teamRun.score}</div>
-        <div class="col">${teamRun.run.roster[0].character.name},${teamRun.run.roster[1].character.name}</div>
+        <div class="col-2"><a href="https://raider.io${teamRun.run.roster[0].character.path}">${teamRun.run.roster[0].character.name}</a>,<a href=https://raider.io${teamRun.run.roster[1].character.path}>${teamRun.run.roster[1].character.name}</a></div>
+        <div class="col-2"><a href=https://raider.io${teamRun.run.roster[2].character.path}>${teamRun.run.roster[2].character.name}</a>,<a href=https://raider.io${teamRun.run.roster[3].character.path}>${teamRun.run.roster[3].character.name}</a></div>
+        <div class="col-1"><a href=https://raider.io${teamRun.run.roster[4].character.path}>${teamRun.run.roster[4].character.name}</a></div>
         `
         mythicRankings.appendChild(runListItem)
     }
 }
 displayRio();
-
-//<div class="col">${teamRun.run.weekly_modifiers[0].name},${teamRun.run.weekly_modifiers[1].name},${teamRun.run.weekly_modifiers[2].name},${teamRun.run.weekly_modifiers[3].name}</div>
